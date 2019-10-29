63f53a26c2042dc03e51cd2596c44cb8b12cfe55
Minh Nghia <minhmeo0507@gmail.com>
2019-10-29 11:30:21 +0700

    Chỉnh sửa CSDL

---
+++ View/Resource/libro.sql
@@ -0,0 +1,229 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th10 28, 2019 lúc 02:54 PM
+-- Phiên bản máy phục vụ: 10.4.6-MariaDB
+-- Phiên bản PHP: 7.3.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `libro`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` bigint(10) NOT NULL,
+  `user_id` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `total_price` int(11) NOT NULL,
+  `success_state` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `order_content`
+--
+
+CREATE TABLE `order_content` (
+  `order_id` bigint(10) NOT NULL,
+  `product_id` bigint(10) NOT NULL,
+  `amount` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `category` bigint(10) NOT NULL,
+  `price` int(11) NOT NULL,
+  `sale` float NOT NULL,
+  `image_link` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `sell_state` bit(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `product_categories`
+--
+
+CREATE TABLE `product_categories` (
+  `id` bigint(10) NOT NULL,
+  `name` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `shipping_info`
+--
+
+CREATE TABLE `shipping_info` (
+  `order_id` bigint(10) NOT NULL,
+  `firstname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `country` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `county` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `province` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `street_address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `postcode` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `tel` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `notes` text COLLATE utf8mb4_unicode_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `user_name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `pass` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `firstname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lastname` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `county` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `province` varchar(30) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `street_address` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `phone_number` varchar(20) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orders_fk0` (`user_id`);
+
+--
+-- Chỉ mục cho bảng `order_content`
+--
+ALTER TABLE `order_content`
+  ADD PRIMARY KEY (`order_id`,`product_id`),
+  ADD KEY `order_content_fk1` (`product_id`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `products_fk0` (`category`);
+
+--
+-- Chỉ mục cho bảng `product_categories`
+--
+ALTER TABLE `product_categories`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `name` (`name`);
+
+--
+-- Chỉ mục cho bảng `shipping_info`
+--
+ALTER TABLE `shipping_info`
+  ADD PRIMARY KEY (`order_id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id` (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `product_categories`
+--
+ALTER TABLE `product_categories`
+  MODIFY `id` bigint(10) NOT NULL AUTO_INCREMENT;
+
+--
+-- Các ràng buộc cho các bảng đã đổ
+--
+
+--
+-- Các ràng buộc cho bảng `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
+
+--
+-- Các ràng buộc cho bảng `order_content`
+--
+ALTER TABLE `order_content`
+  ADD CONSTRAINT `order_content_fk0` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `order_content_fk1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`);
+
+--
+-- Các ràng buộc cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `products_fk0` FOREIGN KEY (`category`) REFERENCES `product_categories` (`id`);
+
+--
+-- Các ràng buộc cho bảng `shipping_info`
+--
+ALTER TABLE `shipping_info`
+  ADD CONSTRAINT `shipping_info_fk0` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
+COMMIT;
+
+insert into product_categories
+values 
+(1,'Chính trị-Pháp luật'),
+(2,'Kiến thức tổng hợp'),
+(3,'Kinh tế-Khoa học công nghệ'),
+(4,'Kỹ năng'),
+(5,'Lịch sử-Văn hóa xã hội'),
+(6,'Ngoại ngữ'),
+(7,'Sức khỏe-Sắc đẹp'),
+(8,'Tâm linh-Tôn giáo'),
+(9,'Thiếu nhi'),
+(10,'Văn học-Nghệ thuật');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
