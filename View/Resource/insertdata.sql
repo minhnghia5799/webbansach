5172adab597d577f7b38cfce460881848a2df3fd
Minh Nghia <minhmeo0507@gmail.com>
2019-11-16 10:39:24 +0700

    insert

--- View/Resource/insertdata.sql
+++ View/Resource/insertdata.sql
@@ -17,457 +17,457 @@ VALUES
 
 (1,'Bộ luật dân sự',1,
 120000,
-95000,'View/Resource/book_img/Chính trị-Pháp luật/sach1.jpg'),
+95000,'View/Resource/book_img/Chính trị-Pháp luật/sach1.jpg',0,0),
 
 ('','Bộ luật hình sự',1,
 132000,
-103000,'View/Resource/book_img/Chính trị-Pháp luật/sach2.jpg'),
+103000,'View/Resource/book_img/Chính trị-Pháp luật/sach2.jpg',0,0),
 
 ('','Bộ luật tố tụng dân sự',1,
 146000,
-127000,'View/Resource/book_img/Chính trị-Pháp luật/sach3.jpg'),
+127000,'View/Resource/book_img/Chính trị-Pháp luật/sach3.jpg',0,0),
 
 ('','Chính trị luận',1,
 105000,
-82000,'View/Resource/book_img/Chính trị-Pháp luật/sach4.jpg'),
+82000,'View/Resource/book_img/Chính trị-Pháp luật/sach4.jpg',0,0),
 
 ('','Giải quyết tranh chấp hợp đồng',1,
 90000,
-77000,'View/Resource/book_img/Chính trị-Pháp luật/sach5.png'),
+77000,'View/Resource/book_img/Chính trị-Pháp luật/sach5.png',0,0),
 
 ('','Hiến pháp',1,
 137000,
-114000,'View/Resource/book_img/Chính trị-Pháp luật/sach6.jpg'),
+114000,'View/Resource/book_img/Chính trị-Pháp luật/sach6.jpg',0,0),
 
 ('','Hồi ký Lý Quang Diệu',1,
 150000,
-129000,'View/Resource/book_img/Chính trị-Pháp luật/sach7.jpg'),
+129000,'View/Resource/book_img/Chính trị-Pháp luật/sach7.jpg',0,0),
 
 ('','Luật doanh nghiệp',1,
 126000,
-101000,'View/Resource/book_img/Chính trị-Pháp luật/sach8.jpg'),
+101000,'View/Resource/book_img/Chính trị-Pháp luật/sach8.jpg',0,0),
 
 ('','Luật thương mại',1,
 139000,
-119000,'View/Resource/book_img/Chính trị-Pháp luật/sach9.jpg'),
+119000,'View/Resource/book_img/Chính trị-Pháp luật/sach9.jpg',0,0),
 
 ('','Quân vương',1,
 87000,
-72000,'View/Resource/book_img/Chính trị-Pháp luật/sach10.jpg'),
+72000,'View/Resource/book_img/Chính trị-Pháp luật/sach10.jpg',0,0),
 
 ('','Tư tưởng chính trị Hồ Chí Minh',1,
 75000,
-61000,'View/Resource/book_img/Chính trị-Pháp luật/sach11.jpg'),
+61000,'View/Resource/book_img/Chính trị-Pháp luật/sach11.jpg',0,0),
 
 ('','Cuộc sống rất giống cuộc đời',2,
 82000,
-69000,'View/Resource/book_img/Kiến thức tổng hợp/sach12.jpg'),
+69000,'View/Resource/book_img/Kiến thức tổng hợp/sach12.jpg',0,0),
 
 ('','Ngôn ngữ cơ thể',2,
 132000,
-103000,'View/Resource/book_img/Kiến thức tổng hợp/sach13.jpg'),
+103000,'View/Resource/book_img/Kiến thức tổng hợp/sach13.jpg',0,0),
 
 ('','Người đan chữ xếp thuyền',2,
 75000,
-61000,'View/Resource/book_img/Kiến thức tổng hợp/sach14.jpg'),
+61000,'View/Resource/book_img/Kiến thức tổng hợp/sach14.jpg',0,0),
 
 ('','Quảy gánh băng đồng ra thế giới',2,
 110000,
-99000,'View/Resource/book_img/Kiến thức tổng hợp/sach15.jpg'),
+99000,'View/Resource/book_img/Kiến thức tổng hợp/sach15.jpg',0,0),
 
 ('','Tâm lý học hài hước',2,
 132000,
-100000,'View/Resource/book_img/Kiến thức tổng hợp/sach16.jpg'),
+100000,'View/Resource/book_img/Kiến thức tổng hợp/sach16.jpg',0,0),
 
 ('','Tôi vẽ',2,
 67000,
-45000,'View/Resource/book_img/Kiến thức tổng hợp/sach17.jpg'),
+45000,'View/Resource/book_img/Kiến thức tổng hợp/sach17.jpg',0,0),
 
 ('','Thuật tư tưởng',2,
 97000,
-77000,'View/Resource/book_img/Kiến thức tổng hợp/sach18.jpg'),
+77000,'View/Resource/book_img/Kiến thức tổng hợp/sach18.jpg',0,0),
 
 ('','Tri thức về vạn vật',2,
 122000,
-103000,'View/Resource/book_img/Kiến thức tổng hợp/sach19.jpg'),
+103000,'View/Resource/book_img/Kiến thức tổng hợp/sach19.jpg',0,0),
 
 ('','Vạn vật vận hành',2,
 86000,
-64000,'View/Resource/book_img/Kiến thức tổng hợp/sach20.jpg'),
+64000,'View/Resource/book_img/Kiến thức tổng hợp/sach20.jpg',0,0),
 
 ('','Vũ trụ trong vỏ hạt dẻ',2,
 137000,
-124000,'View/Resource/book_img/Kiến thức tổng hợp/sach21.jpg'),
+124000,'View/Resource/book_img/Kiến thức tổng hợp/sach21.jpg',0,0),
 
 
 ('','Bí mật thành công của những người bán hàng xuất sắc',3,
 125000,
-103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach22.jpg'),
+103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach22.jpg',0,0),
 
 ('','Các thế giới song song',3,
 98000,
-78000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach23.jpg'),
+78000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach23.jpg',0,0),
 
 ('','Code dạo ký sự',3,
 87000,
-69000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach24.jpg'),
+69000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach24.jpg',0,0),
 
 ('','Để không có tiền vẫn tạo ra tiền',3,
 132000,
-103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach25.jpg'),
+103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach25.jpg',0,0),
 ('','Khởi nghiệp bán lẻ',3,
 152000,
-133000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach26.jpg'),
+133000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach26.jpg',0,0),
 
 ('','Khởi nghiệp tinh gọn',3,
 109000,
-97000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach27.jpg'),
+97000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach27.jpg',0,0),
 
 ('','Khơi nguồn trực giác',3,
 119000,
-104000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach28.jpg'),
+104000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach28.jpg',0,0),
 
 ('','Nhà lãnh đạo không chức danh',3,
 89000,
-71000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach29.jpg'),
+71000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach29.jpg',0,0),
 
 ('','Máu bẩn',3,
 140000,
-129000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach30.jpg'),
+129000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach30.jpg',0,0),
 
 ('','Khoa học',3,
 55000,
-39000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach31.jpg'),
+39000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach31.jpg',0,0),
 
 ('','Amazon - ông vua bán lẻ',3,
 125000,
-98000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach32.jpg'),
+98000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach32.jpg',0,0),
 
 ('','Toán học - Một thiên tiểu thuyết',3,
 97000,
-73000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach33.jpg'),
+73000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach33.jpg',0,0),
 
 ('','Tế bào gốc',3,
 106000,
-88000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach34.jpg'),
+88000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach34.jpg',0,0),
 
 
 
 ('','Tiếp thị 4.0',3,
 132000,
-103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach35.jpg'),
+103000,'View/Resource/book_img/Kinh tế-Khoa học công nghệ/sach35.jpg',0,0),
 
 ('','Bạn đăt giá bao nhiêu',4,
 87000,
-67000,'View/Resource/book_img/Kỹ năng/sach36.jpg'),
+67000,'View/Resource/book_img/Kỹ năng/sach36.jpg',0,0),
 
 ('','Cân bằng cảm xúc cả lúc bão giông',4,
 72000,
-59000,'View/Resource/book_img/Kỹ năng/sach37.jpg'),
+59000,'View/Resource/book_img/Kỹ năng/sach37.jpg',0,0),
 
 ('','Đắc nhân tâm',4,
 164000,
-149000,'View/Resource/book_img/Kỹ năng/sach38.jpg'),
+149000,'View/Resource/book_img/Kỹ năng/sach38.jpg',0,0),
 
 ('','Đừng lựa chọn an nhàn khi còn trẻ',4,
 84000,
-71000,'View/Resource/book_img/Kỹ năng/sach39.jpg'),
+71000,'View/Resource/book_img/Kỹ năng/sach39.jpg',0,0),
 
 ('','Giới hạn của bạn chỉ là xuất phát điểm của tôi',4,
 92000,
-79000,'View/Resource/book_img/Kỹ năng/sach40.jpg'),
+79000,'View/Resource/book_img/Kỹ năng/sach40.jpg',0,0),
 
 ('','Khí chất bao nhiêu hạnh phúc bấy nhiêu',4,
 65000,
-49000,'View/Resource/book_img/Kỹ năng/sach41.jpg'),
+49000,'View/Resource/book_img/Kỹ năng/sach41.jpg',0,0),
 
 ('','Khi hơi thở hóa thinh không',4,
 73000,
-67000,'View/Resource/book_img/Kỹ năng/sach42.jpg'),
+67000,'View/Resource/book_img/Kỹ năng/sach42.jpg',0,0),
 
 
 ('','Nòng giận là bản năng, thinh lặng là bản lĩnh',4,
 84000,
-66000,'View/Resource/book_img/Kỹ năng/sach43.jpg'),
+66000,'View/Resource/book_img/Kỹ năng/sach43.jpg',0,0),
 
 ('','Sống thực tế giữa đời thực dụng',4,
 92000,
-75000,'View/Resource/book_img/Kỹ năng/sach44.jpg'),
+75000,'View/Resource/book_img/Kỹ năng/sach44.jpg',0,0),
 
 ('','Trên đường băng',4,
 118000,
-99000,'View/Resource/book_img/Kỹ năng/sach45.jpg'),
+99000,'View/Resource/book_img/Kỹ năng/sach45.jpg',0,0),
 
 ('','100 gương hiếu học của các doanh nhân nổi tiếng',5,
 88000,
-69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach46.jpg'),
+69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach46.jpg',0,0),
 
 ('','Đại việt sử ký toàn thư',5,
 96000,
-62000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach47.jpg'),
+62000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach47.jpg',0,0),
 
 ('','Đất lề quê thói',5,
 71000,
-59000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach48.jpg'),
+59000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach48.jpg',0,0),
 
 ('','Đối thoại với nền văn minh cổ Champa',5,
 79000,
-57000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach49.jpg'),
+57000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach49.jpg',0,0),
 
 ('','Lược sử loài người',5,
 62000,
-55000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach50.jpg'),
+55000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach50.jpg',0,0),
 
 ('','Lược sử tương lai',5,
 99000,
-73000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach51.jpg'),
+73000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach51.jpg',0,0),
 
 ('','Tập tục đời người',5,
 94000,
-69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach52.jpg'),
+69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach52.jpg',0,0),
 
 ('','Việt Nam phong tục',5,
 11000,
-89000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach53.jpg'),
+89000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach53.jpg',0,0),
 
 ('','Việt Nam sử lược',5,
 82000,
-65000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach54.jpg'),
+65000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach54.jpg',0,0),
 
 ('','Xứ Đông Dương',5,
 84000,
-69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach55.jpg'),
+69000,'View/Resource/book_img/Lịch sử-Văn hóa xã hội/sach55.jpg',0,0),
 
 ('','Cẩm nang tự học IELTS',6,
 79000,
-57000,'View/Resource/book_img/Ngoại ngữ/sach56.jpg'),
+57000,'View/Resource/book_img/Ngoại ngữ/sach56.jpg',0,0),
 
 ('','Giáo trình hán ngữ',6,
 81000,
-69000,'View/Resource/book_img/Ngoại ngữ/sach57.jpg'),
+69000,'View/Resource/book_img/Ngoại ngữ/sach57.jpg',0,0),
 ('','Hack não 1500',6,
 94000,
-79000,'View/Resource/book_img/Ngoại ngữ/sach58.jpg'),
+79000,'View/Resource/book_img/Ngoại ngữ/sach58.jpg',0,0),
 
 ('','Luyện siêu trí nhớ từ vựng tiếng Anh',6,
 84000,
-69000,'View/Resource/book_img/Ngoại ngữ/sach59.jpg'),
+69000,'View/Resource/book_img/Ngoại ngữ/sach59.jpg',0,0),
 
 ('','Ngữ pháp tiếng Anh',6,
 85000,
-69000,'View/Resource/book_img/Ngoại ngữ/sach60.jpg'),
+69000,'View/Resource/book_img/Ngoại ngữ/sach60.jpg',0,0),
 
 ('','Tập viết chữu Hán',6,
 47000,
-39000,'View/Resource/book_img/Ngoại ngữ/sach62.jpg'),
+39000,'View/Resource/book_img/Ngoại ngữ/sach62.jpg',0,0),
 
 
 ('','Tiếng Nhật cho mọi người',6,
 76000,
-69000,'View/Resource/book_img/Ngoại ngữ/sach63.jpg'),
+69000,'View/Resource/book_img/Ngoại ngữ/sach63.jpg',0,0),
 
 ('','Tự học tiếng Trung cho người mới bắt đầu',6,
 70000,
-61000,'View/Resource/book_img/Ngoại ngữ/sach64.jpg'),
+61000,'View/Resource/book_img/Ngoại ngữ/sach64.jpg',0,0),
 
 ('','Vừa lười vừa bận vẫn giỏi tiếng Anh',6,
 89000,
-67000,'View/Resource/book_img/Ngoại ngữ/sach65.jpg'),
+67000,'View/Resource/book_img/Ngoại ngữ/sach65.jpg',0,0),
 
 ('','Axit và kiềm trong thực dưỡng',7,
 76000,
-69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach66.jpg'),
+69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach66.jpg',0,0),
 
 ('','Dưỡng da trọn gói',7,
 68000,
-47000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach67.jpg'),
+47000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach67.jpg',0,0),
 
 ('','Hạt giống tâm hồn',7,
 43000,
-33000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach68.jpg'),
+33000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach68.jpg',0,0),
 
 ('','Hiểu hết về cơ thể',7,
 59000,
-42000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach69.jpg'),
+42000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach69.jpg',0,0),
 
 ('','Ngủ ít vẫn khỏe',7,
 68000,
-49000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach70.jpg'),
+49000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach70.jpg',0,0),
 
 ('','Phương thức sống lành mạnh',7,
 74000,
-61000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach71.jpg'),
+61000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach71.jpg',0,0),
 
 ('','Thần dược xanh',7,
 69000,
-47000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach72.jpg'),
+47000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach72.jpg',0,0),
 
 
 ('','Thân nhiệt - Chìa khóa để sống khỏe mạnh và trường thọ',7,
 76000,
-69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach73.jpg'),
+69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach73.jpg',0,0),
 
 
 ('','Thánh kinh dưỡng da',7,
 89000,
-63000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach74.jpg'),
+63000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach74.jpg',0,0),
 
 ('','Ung thư - Tin đồn và sự thật',7,
 82000,
-69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach75.jpg'),
+69000,'View/Resource/book_img/Sức khỏe-Sắc đẹp/sach75.jpg',0,0),
 
 ('','Đạo giáo',8,
 57000,
-45000,'View/Resource/book_img/Tâm linh tôn giáo/sach76.jpg'),
+45000,'View/Resource/book_img/Tâm linh tôn giáo/sach76.jpg',0,0),
 
 ('','Đạo Phật vỡ lòng',8,
 54000,
-41000,'View/Resource/book_img/Tâm linh tôn giáo/sach77.jpg'),
+41000,'View/Resource/book_img/Tâm linh tôn giáo/sach77.jpg',0,0),
 
 ('','Gieo trồng hạn phúc',8,
 67000,
-52000,'View/Resource/book_img/Tâm linh tôn giáo/sach78.jpg'),
+52000,'View/Resource/book_img/Tâm linh tôn giáo/sach78.jpg',0,0),
 
 ('','Hành trình về phương đông',8,
 53000,
-45000,'View/Resource/book_img/Tâm linh tôn giáo/sach79.jpg'),
+45000,'View/Resource/book_img/Tâm linh tôn giáo/sach79.jpg',0,0),
 
 ('','Minh triết trong đời sống',8,
 56000,
-45000,'View/Resource/book_img/Tâm linh tôn giáo/sach80.jpg'),
+45000,'View/Resource/book_img/Tâm linh tôn giáo/sach80.jpg',0,0),
 
 ('','Mở lối yêu thương',8,
 65000,
-48000,'View/Resource/book_img/Tâm linh tôn giáo/sach81.jpg'),
+48000,'View/Resource/book_img/Tâm linh tôn giáo/sach81.jpg',0,0),
 
 ('','Sống như nhân duyên, nghệ thuật nhìn người',8,
 67000,
-59000,'View/Resource/book_img/Tâm linh tôn giáo/sach82.jpg'),
+59000,'View/Resource/book_img/Tâm linh tôn giáo/sach82.jpg',0,0),
 
 ('','Tĩnh lặng',8,
 57000,
-45000,'View/Resource/book_img/Tâm linh tôn giáo/sach83.jpg'),
+45000,'View/Resource/book_img/Tâm linh tôn giáo/sach83.jpg',0,0),
 
 ('','Tứ diệu đế',8,
 54000,
-49000,'View/Resource/book_img/Tâm linh tôn giáo/sach84.jpg'),
+49000,'View/Resource/book_img/Tâm linh tôn giáo/sach84.jpg',0,0),
 
 ('','Vô ngã vô ưu',8,
 69000,
-57000,'View/Resource/book_img/Tâm linh tôn giáo/sach85.jpg'),
+57000,'View/Resource/book_img/Tâm linh tôn giáo/sach85.jpg',0,0),
 
 ('','Nuôi dưỡng',9,
 49000,
-36000,'View/Resource/book_img/Thiếu nhi/sach86.jpg'),
+36000,'View/Resource/book_img/Thiếu nhi/sach86.jpg',0,0),
 
 ('','Cẩm nang con gái',9,
 47000,
-31000,'View/Resource/book_img/Thiếu nhi/sach87.jpg'),
+31000,'View/Resource/book_img/Thiếu nhi/sach87.jpg',0,0),
 
 ('','Cẩm nang con trai',9,
 47000,
-31000,'View/Resource/book_img/Thiếu nhi/sach88.jpg'),
+31000,'View/Resource/book_img/Thiếu nhi/sach88.jpg',0,0),
 
 ('','Chú thỏ tinh khôn',9,
 48000,
-36000,'View/Resource/book_img/Thiếu nhi/sach89.jpg'),
+36000,'View/Resource/book_img/Thiếu nhi/sach89.jpg',0,0),
 
 ('','Chúng tớ lạc vào cuốn sách khác',9,
 37000,
-29000,'View/Resource/book_img/Thiếu nhi/sach90.jpg'),
+29000,'View/Resource/book_img/Thiếu nhi/sach90.jpg',0,0),
 
 ('','Chuyện con mèo dạy hải âu bay',9,
 39000,
-32000,'View/Resource/book_img/Thiếu nhi/sach91.jpg'),
+32000,'View/Resource/book_img/Thiếu nhi/sach91.jpg',0,0),
 
 ('','Có hai con mèo ngồi bên cửa sổ',9,
 58000,
-45000,'View/Resource/book_img/Thiếu nhi/sach92.jpg'),
+45000,'View/Resource/book_img/Thiếu nhi/sach92.jpg',0,0),
 
 ('','Cơ thể con là của con',9,
 45000,
-29000,'View/Resource/book_img/Thiếu nhi/sach93.jpg'),
+29000,'View/Resource/book_img/Thiếu nhi/sach93.jpg',0,0),
 
 ('','Cá vàng trốn ở đâu rồi nhỉ',9,
 36000,
-21000,'View/Resource/book_img/Thiếu nhi/sach94.jpg'),
+21000,'View/Resource/book_img/Thiếu nhi/sach94.jpg',0,0),
 
 ('','Hoàng tử bé',9,
 67000,
-51000,'View/Resource/book_img/Thiếu nhi/sach95.jpg'),
+51000,'View/Resource/book_img/Thiếu nhi/sach95.jpg',0,0),
 
 ('','Mẹ yêu ai nhất?',9,
 46000,
-32000,'View/Resource/book_img/Thiếu nhi/sach96.jpg'),
+32000,'View/Resource/book_img/Thiếu nhi/sach96.jpg',0,0),
 
 ('','Toán 4',9,
 49000,
-36000,'View/Resource/book_img/Thiếu nhi/sach97.jpg'),
+36000,'View/Resource/book_img/Thiếu nhi/sach97.jpg',0,0),
 
 ('','Siêu nhân khoai',9,
 39000,
-30000,'View/Resource/book_img/Thiếu nhi/sach98.jpg'),
+30000,'View/Resource/book_img/Thiếu nhi/sach98.jpg',0,0),
 
 ('','Siêu thỏ',9,
 35000,
-23000,'View/Resource/book_img/Thiếu nhi/sach99.jpg'),
+23000,'View/Resource/book_img/Thiếu nhi/sach99.jpg',0,0),
 
 ('','Bí ẩn làng ma sói',10,
 64000,
-59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach100.jpg'),
+59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach100.jpg',0,0),
 
 ('','Bố già',10,
 76000,
-63000,'View/Resource/book_img/Văn học-Nghệ thuật/sach101.jpg'),
+63000,'View/Resource/book_img/Văn học-Nghệ thuật/sach101.jpg',0,0),
 
 ('','Chiến binh cầu vồng',10,
 69000,
-52000,'View/Resource/book_img/Văn học-Nghệ thuật/sach102.jpg'),
+52000,'View/Resource/book_img/Văn học-Nghệ thuật/sach102.jpg',0,0),
 
 ('','Cho xin một vé đi tuổi thơ',10,
 75000,
-61000,'View/Resource/book_img/Văn học-Nghệ thuật/sach103.jpg'),
+61000,'View/Resource/book_img/Văn học-Nghệ thuật/sach103.jpg',0,0),
 
 ('','Cuốn theo chiều gió',10,
 67000,
-59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach104.jpg'),
+59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach104.jpg',0,0),
 
 ('','Thất tịch không mưa',10,
 72000,
-57000,'View/Resource/book_img/Văn học-Nghệ thuật/sach105.jpg'),
+57000,'View/Resource/book_img/Văn học-Nghệ thuật/sach105.jpg',0,0),
 
 ('','Hai số phận',10,
 74000,
-59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach106.jpg'),
+59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach106.jpg',0,0),
 
 ('','Không gia đình',10,
 117000,
-89000,'View/Resource/book_img/Văn học-Nghệ thuật/sach107.jpg'),
+89000,'View/Resource/book_img/Văn học-Nghệ thuật/sach107.jpg',0,0),
 
 ('','Kiêu hãnh và định kiến',10,
 69000,
-53000,'View/Resource/book_img/Văn học-Nghệ thuật/sach108.jpg'),
+53000,'View/Resource/book_img/Văn học-Nghệ thuật/sach108.jpg',0,0),
 
 ('','Mắt biếc',10,
 73000,
-59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach109.jpg'),
+59000,'View/Resource/book_img/Văn học-Nghệ thuật/sach109.jpg',0,0),
 
 ('','Nếu biết trăm năm là hữu hạn',10,
 75000,
-63000,'View/Resource/book_img/Văn học-Nghệ thuật/sach110.jpg'),
+63000,'View/Resource/book_img/Văn học-Nghệ thuật/sach110.jpg',0,0),
 
 
 ('','Nhà giả kim',10,
 79000,
-61000,'View/Resource/book_img/Văn học-Nghệ thuật/sach111.jpg'),
+61000,'View/Resource/book_img/Văn học-Nghệ thuật/sach111.jpg',0,0),
 
 ('','Những tấm lòng cao cả',10,
 87000,
-64000,'View/Resource/book_img/Văn học-Nghệ thuật/sach112.jpg'),
+64000,'View/Resource/book_img/Văn học-Nghệ thuật/sach112.jpg',0,0),
 
 ('','Phía sau nghi can',10,
 83000,
-69000,'View/Resource/book_img/Văn học-Nghệ thuật/sach113.jpg');
+69000,'View/Resource/book_img/Văn học-Nghệ thuật/sach113.jpg',0,0);
 
 
 
