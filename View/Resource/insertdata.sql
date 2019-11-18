0fa331a8a71b10ce3729286d5f1f0e0056849a1b
Thiên Ân <ndsg1964@gmail.com>
2019-11-18 21:30:02 +0700

    fix data

--- View/Resource/insertdata.sql
+++ View/Resource/insertdata.sql
@@ -19,453 +19,450 @@ VALUES
 120000,
 95000,'View/Resource/book_img/ChinhTri-PhapLuat/sach1.jpg',3,0),
 
-('','Bộ luật hình sự',1,
+(2,'Bộ luật hình sự',1,
 132000,
 103000,'View/Resource/book_img/ChinhTri-PhapLuat/sach2.jpg',2,0),
 
-('','Bộ luật tố tụng dân sự',1,
+(3,'Bộ luật tố tụng dân sự',1,
 146000,
 127000,'View/Resource/book_img/ChinhTri-PhapLuat/sach3.jpg',1,0),
 
-('','Chính trị luận',1,
+(4,'Chính trị luận',1,
 105000,
 82000,'View/Resource/book_img/ChinhTri-PhapLuat/sach4.jpg',0,4),
 
-('','Giải quyết tranh chấp hợp đồng',1,
+(5,'Giải quyết tranh chấp hợp đồng',1,
 90000,
 77000,'View/Resource/book_img/ChinhTri-PhapLuat/sach5.png',0,3),
 
-('','Hiến pháp',1,
+(6,'Hiến pháp',1,
 137000,
 114000,'View/Resource/book_img/ChinhTri-PhapLuat/sach6.jpg',0,2),
 
-('','Hồi ký Lý Quang Diệu',1,
+(7,'Hồi ký Lý Quang Diệu',1,
 150000,
 129000,'View/Resource/book_img/ChinhTri-PhapLuat/sach7.jpg',0,0),
 
-('','Luật doanh nghiệp',1,
+(8,'Luật doanh nghiệp',1,
 126000,
 101000,'View/Resource/book_img/ChinhTri-PhapLuat/sach8.jpg',0,0),
 
-('','Luật thương mại',1,
+(9,'Luật thương mại',1,
 139000,
 119000,'View/Resource/book_img/ChinhTri-PhapLuat/sach9.jpg',0,0),
 
-('','Quân vương',1,
+(10,'Quân vương',1,
 87000,
 72000,'View/Resource/book_img/ChinhTri-PhapLuat/sach10.jpg',0,0),
 
-('','Tư tưởng chính trị Hồ Chí Minh',1,
+(11,'Tư tưởng chính trị Hồ Chí Minh',1,
 75000,
 61000,'View/Resource/book_img/ChinhTri-PhapLuat/sach11.jpg',0,0),
 
-('','Cuộc sống rất giống cuộc đời',2,
+(12,'Cuộc sống rất giống cuộc đời',2,
 82000,
 69000,'View/Resource/book_img/KienThucTongHop/sach12.jpg',0,0),
 
-('','Ngôn ngữ cơ thể',2,
+(13,'Ngôn ngữ cơ thể',2,
 132000,
 103000,'View/Resource/book_img/KienThucTongHop/sach13.jpg',0,0),
 
-('','Người đan chữ xếp thuyền',2,
+(14,'Người đan chữ xếp thuyền',2,
 75000,
 61000,'View/Resource/book_img/KienThucTongHop/sach14.jpg',0,0),
 
-('','Quảy gánh băng đồng ra thế giới',2,
+(15,'Quảy gánh băng đồng ra thế giới',2,
 110000,
 99000,'View/Resource/book_img/KienThucTongHop/sach15.jpg',0,0),
 
-('','Tâm lý học hài hước',2,
+(16,'Tâm lý học hài hước',2,
 132000,
 100000,'View/Resource/book_img/KienThucTongHop/sach16.jpg',0,0),
 
-('','Tôi vẽ',2,
+(17,'Tôi vẽ',2,
 67000,
 45000,'View/Resource/book_img/KienThucTongHop/sach17.jpg',0,0),
 
-('','Thuật tư tưởng',2,
+(18,'Thuật tư tưởng',2,
 97000,
 77000,'View/Resource/book_img/KienThucTongHop/sach18.jpg',0,0),
 
-('','Tri thức về vạn vật',2,
+(19,'Tri thức về vạn vật',2,
 122000,
 103000,'View/Resource/book_img/KienThucTongHop/sach19.jpg',0,0),
 
-('','Vạn vật vận hành',2,
+(20,'Vạn vật vận hành',2,
 86000,
 64000,'View/Resource/book_img/KienThucTongHop/sach20.jpg',0,0),
 
-('','Vũ trụ trong vỏ hạt dẻ',2,
+(21,'Vũ trụ trong vỏ hạt dẻ',2,
 137000,
 124000,'View/Resource/book_img/KienThucTongHop/sach21.jpg',0,0),
 
 
-('','Bí mật thành công của những người bán hàng xuất sắc',3,
+(22,'Bí mật thành công của những người bán hàng xuất sắc',3,
 125000,
 103000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach22.jpg',0,0),
 
-('','Các thế giới song song',3,
+(23,'Các thế giới song song',3,
 98000,
 78000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach23.jpg',0,0),
 
-('','Code dạo ký sự',3,
+(24,'Code dạo ký sự',3,
 87000,
 69000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach24.jpg',0,0),
 
-('','Để không có tiền vẫn tạo ra tiền',3,
+(25,'Để không có tiền vẫn tạo ra tiền',3,
 132000,
 103000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach25.jpg',0,0),
-('','Khởi nghiệp bán lẻ',3,
+
+(26,'Khởi nghiệp bán lẻ',3,
 152000,
 133000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach26.jpg',0,0),
 
-('','Khởi nghiệp tinh gọn',3,
+(27,'Khởi nghiệp tinh gọn',3,
 109000,
 97000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach27.jpg',0,0),
 
-('','Khơi nguồn trực giác',3,
+(28,'Khơi nguồn trực giác',3,
 119000,
 104000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach28.jpg',0,0),
 
-('','Nhà lãnh đạo không chức danh',3,
+(29,'Nhà lãnh đạo không chức danh',3,
 89000,
 71000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach29.jpg',0,0),
 
-('','Máu bẩn',3,
+(30,'Máu bẩn',3,
 140000,
 129000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach30.jpg',0,0),
 
-('','Khoa học',3,
+(31,'Khoa học',3,
 55000,
 39000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach31.jpg',0,0),
 
-('','Amazon - ông vua bán lẻ',3,
+(32,'Amazon - ông vua bán lẻ',3,
 125000,
 98000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach32.jpg',0,0),
 
-('','Toán học - Một thiên tiểu thuyết',3,
+(33,'Toán học - Một thiên tiểu thuyết',3,
 97000,
 73000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach33.jpg',0,0),
 
-('','Tế bào gốc',3,
+(34,'Tế bào gốc',3,
 106000,
 88000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach34.jpg',0,0),
 
-
-
-('','Tiếp thị 4.0',3,
+(35,'Tiếp thị 4.0',3,
 132000,
 103000,'View/Resource/book_img/KinhTe-KhoaHocCongNghe/sach35.jpg',0,0),
 
-('','Bạn đăt giá bao nhiêu',4,
+(36,'Bạn đăt giá bao nhiêu',4,
 87000,
 67000,'View/Resource/book_img/KyNang/sach36.jpg',0,0),
 
-('','Cân bằng cảm xúc cả lúc bão giông',4,
+(37,'Cân bằng cảm xúc cả lúc bão giông',4,
 72000,
 59000,'View/Resource/book_img/KyNang/sach37.jpg',0,0),
 
-('','Đắc nhân tâm',4,
+(38,'Đắc nhân tâm',4,
 164000,
 149000,'View/Resource/book_img/KyNang/sach38.jpg',0,0),
 
-('','Đừng lựa chọn an nhàn khi còn trẻ',4,
+(39,'Đừng lựa chọn an nhàn khi còn trẻ',4,
 84000,
 71000,'View/Resource/book_img/KyNang/sach39.jpg',0,0),
 
-('','Giới hạn của bạn chỉ là xuất phát điểm của tôi',4,
+(40,'Giới hạn của bạn chỉ là xuất phát điểm của tôi',4,
 92000,
 79000,'View/Resource/book_img/KyNang/sach40.jpg',0,0),
 
-('','Khí chất bao nhiêu hạnh phúc bấy nhiêu',4,
+(41,'Khí chất bao nhiêu hạnh phúc bấy nhiêu',4,
 65000,
 49000,'View/Resource/book_img/KyNang/sach41.jpg',0,0),
 
-('','Khi hơi thở hóa thinh không',4,
+(42,'Khi hơi thở hóa thinh không',4,
 73000,
 67000,'View/Resource/book_img/KyNang/sach42.jpg',0,0),
 
-
-('','Nòng giận là bản năng, thinh lặng là bản lĩnh',4,
+(43,'Nòng giận là bản năng, thinh lặng là bản lĩnh',4,
 84000,
 66000,'View/Resource/book_img/KyNang/sach43.jpg',0,0),
 
-('','Sống thực tế giữa đời thực dụng',4,
+(44,'Sống thực tế giữa đời thực dụng',4,
 92000,
 75000,'View/Resource/book_img/KyNang/sach44.jpg',0,0),
 
-('','Trên đường băng',4,
+(45,'Trên đường băng',4,
 118000,
 99000,'View/Resource/book_img/KyNang/sach45.gif',0,0),
 
-('','100 gương hiếu học của các doanh nhân nổi tiếng',5,
+(46,'100 gương hiếu học của các doanh nhân nổi tiếng',5,
 88000,
 69000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach46.jpg',0,0),
 
-('','Đại việt sử ký toàn thư',5,
+(47,'Đại việt sử ký toàn thư',5,
 96000,
 62000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach47.jpg',0,0),
 
-('','Đất lề quê thói',5,
+(48,'Đất lề quê thói',5,
 71000,
 59000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach48.png',0,0),
 
-('','Đối thoại với nền văn minh cổ Champa',5,
+(49,'Đối thoại với nền văn minh cổ Champa',5,
 79000,
 57000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach49.jpg',0,0),
 
-('','Lược sử loài người',5,
+(50,'Lược sử loài người',5,
 62000,
 55000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach50.jpg',0,0),
 
-('','Lược sử tương lai',5,
+(51,'Lược sử tương lai',5,
 99000,
 73000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach51.jpg',0,0),
 
-('','Tập tục đời người',5,
+(52,'Tập tục đời người',5,
 94000,
 69000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach52.jpg',0,0),
 
-('','Việt Nam phong tục',5,
+(53,'Việt Nam phong tục',5,
 11000,
 89000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach53.jpg',0,0),
 
-('','Việt Nam sử lược',5,
+(54,'Việt Nam sử lược',5,
 82000,
 65000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach54.jpg',0,0),
 
-('','Xứ Đông Dương',5,
+(55,'Xứ Đông Dương',5,
 84000,
 69000,'View/Resource/book_img/LichSu-VanHoaXaHoi/sach55.jpg',0,0),
 
-('','Cẩm nang tự học IELTS',6,
+(56,'Cẩm nang tự học IELTS',6,
 79000,
 57000,'View/Resource/book_img/NgoaiNgu/sach56.jpg',0,0),
 
-('','Giáo trình hán ngữ',6,
+(57,'Giáo trình hán ngữ',6,
 81000,
 69000,'View/Resource/book_img/NgoaiNgu/sach57.png',0,0),
-('','Hack não 1500',6,
+
+(58,'Hack não 1500',6,
 94000,
 79000,'View/Resource/book_img/NgoaiNgu/sach58.png',0,0),
 
-('','Luyện siêu trí nhớ từ vựng tiếng Anh',6,
+(59,'Luyện siêu trí nhớ từ vựng tiếng Anh',6,
 84000,
 69000,'View/Resource/book_img/NgoaiNgu/sach59.jpg',0,0),
 
-('','Ngữ pháp tiếng Anh',6,
+(60,'Ngữ pháp tiếng Anh',6,
 85000,
 69000,'View/Resource/book_img/NgoaiNgu/sach60.jpg',0,0),
 
-('','Tập viết chữu Hán',6,
+(61,'Tập viết chữu Hán',6,
 47000,
 39000,'View/Resource/book_img/NgoaiNgu/sach62.jpg',0,0),
 
-
-('','Tiếng Nhật cho mọi người',6,
+(62,'Tiếng Nhật cho mọi người',6,
 76000,
 69000,'View/Resource/book_img/NgoaiNgu/sach63.jpg',0,0),
 
-('','Tự học tiếng Trung cho người mới bắt đầu',6,
+(63,'Tự học tiếng Trung cho người mới bắt đầu',6,
 70000,
 61000,'View/Resource/book_img/NgoaiNgu/sach64.png',0,0),
 
-('','Vừa lười vừa bận vẫn giỏi tiếng Anh',6,
+(64,'Vừa lười vừa bận vẫn giỏi tiếng Anh',6,
 89000,
 67000,'View/Resource/book_img/NgoaiNgu/sach65.jpg',0,0),
 
-('','Axit và kiềm trong thực dưỡng',7,
+(65,'Axit và kiềm trong thực dưỡng',7,
 76000,
 69000,'View/Resource/book_img/SucKhoe-SacDep/sach66.jpg',0,0),
 
-('','Dưỡng da trọn gói',7,
+(66,'Dưỡng da trọn gói',7,
 68000,
 47000,'View/Resource/book_img/SucKhoe-SacDep/sach67.jpg',0,0),
 
-('','Hạt giống tâm hồn',7,
+(67,'Hạt giống tâm hồn',7,
 43000,
 33000,'View/Resource/book_img/SucKhoe-SacDep/sach68.jpg',0,0),
 
-('','Hiểu hết về cơ thể',7,
+(68,'Hiểu hết về cơ thể',7,
 59000,
 42000,'View/Resource/book_img/SucKhoe-SacDep/sach69.jpg',0,0),
 
-('','Ngủ ít vẫn khỏe',7,
+(69,'Ngủ ít vẫn khỏe',7,
 68000,
 49000,'View/Resource/book_img/SucKhoe-SacDep/sach70.jpg',0,0),
 
-('','Phương thức sống lành mạnh',7,
+(70,'Phương thức sống lành mạnh',7,
 74000,
 61000,'View/Resource/book_img/SucKhoe-SacDep/sach71.jpg',0,0),
 
-('','Thần dược xanh',7,
+(71,'Thần dược xanh',7,
 69000,
 47000,'View/Resource/book_img/SucKhoe-SacDep/sach72.jpg',0,0),
 
 
-('','Thân nhiệt - Chìa khóa để sống khỏe mạnh và trường thọ',7,
+(72,'Thân nhiệt - Chìa khóa để sống khỏe mạnh và trường thọ',7,
 76000,
 69000,'View/Resource/book_img/SucKhoe-SacDep/sach73.jpg',0,0),
 
 
-('','Thánh kinh dưỡng da',7,
+(73,'Thánh kinh dưỡng da',7,
 89000,
 63000,'View/Resource/book_img/SucKhoe-SacDep/sach74.jpg',0,0),
 
-('','Ung thư - Tin đồn và sự thật',7,
+(74,'Ung thư - Tin đồn và sự thật',7,
 82000,
 69000,'View/Resource/book_img/SucKhoe-SacDep/sach75.jpg',0,0),
 
-('','Đạo giáo',8,
+(75,'Đạo giáo',8,
 57000,
 45000,'View/Resource/book_img/TamLinhTonGiao/sach76.jpg',0,0),
 
-('','Đạo Phật vỡ lòng',8,
+(76,'Đạo Phật vỡ lòng',8,
 54000,
 41000,'View/Resource/book_img/TamLinhTonGiao/sach77.jpg',0,0),
 
-('','Gieo trồng hạn phúc',8,
+(77,'Gieo trồng hạn phúc',8,
 67000,
 52000,'View/Resource/book_img/TamLinhTonGiao/sach78.jpg',0,0),
 
-('','Hành trình về phương đông',8,
+(78,'Hành trình về phương đông',8,
 53000,
 45000,'View/Resource/book_img/TamLinhTonGiao/sach79.jpg',0,0),
 
-('','Minh triết trong đời sống',8,
+(79,'Minh triết trong đời sống',8,
 56000,
 45000,'View/Resource/book_img/TamLinhTonGiao/sach80.jpg',0,0),
 
-('','Mở lối yêu thương',8,
+(80,'Mở lối yêu thương',8,
 65000,
 48000,'View/Resource/book_img/TamLinhTonGiao/sach81.jpg',0,0),
 
-('','Sống như nhân duyên, nghệ thuật nhìn người',8,
+(81,'Sống như nhân duyên, nghệ thuật nhìn người',8,
 67000,
 59000,'View/Resource/book_img/TamLinhTonGiao/sach82.jpg',0,0),
 
-('','Tĩnh lặng',8,
+(82,'Tĩnh lặng',8,
 57000,
 45000,'View/Resource/book_img/TamLinhTonGiao/sach83.jpg',0,0),
 
-('','Tứ diệu đế',8,
+(83,'Tứ diệu đế',8,
 54000,
 49000,'View/Resource/book_img/TamLinhTonGiao/sach84.jpg',0,0),
 
-('','Vô ngã vô ưu',8,
+(84,'Vô ngã vô ưu',8,
 69000,
 57000,'View/Resource/book_img/TamLinhTonGiao/sach85.jpg',0,0),
 
-('','Nuôi dưỡng',9,
+(85,'Nuôi dưỡng',9,
 49000,
 36000,'View/Resource/book_img/ThieuNhi/sach86.jpg',0,0),
 
-('','Cẩm nang con gái',9,
+(86,'Cẩm nang con gái',9,
 47000,
 31000,'View/Resource/book_img/ThieuNhi/sach87.jpg',0,0),
 
-('','Cẩm nang con trai',9,
+(87,'Cẩm nang con trai',9,
 47000,
 31000,'View/Resource/book_img/ThieuNhi/sach88.jpg',0,0),
 
-('','Chú thỏ tinh khôn',9,
+(88,'Chú thỏ tinh khôn',9,
 48000,
 36000,'View/Resource/book_img/ThieuNhi/sach89.jpg',0,0),
 
-('','Chúng tớ lạc vào cuốn sách khác',9,
+(89,'Chúng tớ lạc vào cuốn sách khác',9,
 37000,
 29000,'View/Resource/book_img/ThieuNhi/sach90.jpg',0,0),
 
-('','Chuyện con mèo dạy hải âu bay',9,
+(90,'Chuyện con mèo dạy hải âu bay',9,
 39000,
 32000,'View/Resource/book_img/ThieuNhi/sach91.jpg',0,0),
 
-('','Có hai con mèo ngồi bên cửa sổ',9,
+(91,'Có hai con mèo ngồi bên cửa sổ',9,
 58000,
 45000,'View/Resource/book_img/ThieuNhi/sach92.jpg',0,0),
 
-('','Cơ thể con là của con',9,
+(92,'Cơ thể con là của con',9,
 45000,
 29000,'View/Resource/book_img/ThieuNhi/sach93.jpg',0,0),
 
-('','Cá vàng trốn ở đâu rồi nhỉ',9,
+(93,'Cá vàng trốn ở đâu rồi nhỉ',9,
 36000,
 21000,'View/Resource/book_img/ThieuNhi/sach94.jpg',0,0),
 
-('','Hoàng tử bé',9,
+(94,'Hoàng tử bé',9,
 67000,
 51000,'View/Resource/book_img/ThieuNhi/sach95.jpg',0,0),
 
-('','Mẹ yêu ai nhất?',9,
+(95,'Mẹ yêu ai nhất?',9,
 46000,
 32000,'View/Resource/book_img/ThieuNhi/sach96.jpg',0,0),
 
-('','Toán 4',9,
+(96,'Toán 4',9,
 49000,
 36000,'View/Resource/book_img/ThieuNhi/sach97.jpg',0,0),
 
-('','Siêu nhân khoai',9,
+(97,'Siêu nhân khoai',9,
 39000,
 30000,'View/Resource/book_img/ThieuNhi/sach98.jpg',0,0),
 
-('','Siêu thỏ',9,
+(98,'Siêu thỏ',9,
 35000,
 23000,'View/Resource/book_img/ThieuNhi/sach99.jpg',0,0),
 
-('','Bí ẩn làng ma sói',10,
+(99,'Bí ẩn làng ma sói',10,
 64000,
 59000,'View/Resource/book_img/VanHoc-NgheThuat/sach100.png',0,0),
 
-('','Bố già',10,
+(100,'Bố già',10,
 76000,
 63000,'View/Resource/book_img/VanHoc-NgheThuat/sach101.jpg',0,0),
 
-('','Chiến binh cầu vồng',10,
+(101,'Chiến binh cầu vồng',10,
 69000,
 52000,'View/Resource/book_img/VanHoc-NgheThuat/sach102.jpg',0,0),
 
-('','Cho xin một vé đi tuổi thơ',10,
+(102,'Cho xin một vé đi tuổi thơ',10,
 75000,
 61000,'View/Resource/book_img/VanHoc-NgheThuat/sach103.jpg',0,0),
 
-('','Cuốn theo chiều gió',10,
+(103,'Cuốn theo chiều gió',10,
 67000,
 59000,'View/Resource/book_img/VanHoc-NgheThuat/sach104.jpg',0,0),
 
-('','Thất tịch không mưa',10,
+(104,'Thất tịch không mưa',10,
 72000,
 57000,'View/Resource/book_img/VanHoc-NgheThuat/sach105.jpg',0,0),
 
-('','Hai số phận',10,
+(105,'Hai số phận',10,
 74000,
 59000,'View/Resource/book_img/VanHoc-NgheThuat/sach106.jpg',0,0),
 
-('','Không gia đình',10,
+(106,'Không gia đình',10,
 117000,
 89000,'View/Resource/book_img/VanHoc-NgheThuat/sach107.jpg',0,0),
 
-('','Kiêu hãnh và định kiến',10,
+(107,'Kiêu hãnh và định kiến',10,
 69000,
 53000,'View/Resource/book_img/VanHoc-NgheThuat/sach108.jpg',0,0),
 
-('','Mắt biếc',10,
+(108,'Mắt biếc',10,
 73000,
 59000,'View/Resource/book_img/VanHoc-NgheThuat/sach109.jpg',0,0),
 
-('','Nếu biết trăm năm là hữu hạn',10,
+(109,'Nếu biết trăm năm là hữu hạn',10,
 75000,
 63000,'View/Resource/book_img/VanHoc-NgheThuat/sach110.jpg',0,0),
 
-
-('','Nhà giả kim',10,
+(110,'Nhà giả kim',10,
 79000,
 61000,'View/Resource/book_img/VanHoc-NgheThuat/sach111.jpg',0,0),
 
-('','Những tấm lòng cao cả',10,
+(111,'Những tấm lòng cao cả',10,
 87000,
 64000,'View/Resource/book_img/VanHoc-NgheThuat/sach112.jpg',0,0),
 
-('','Phía sau nghi can',10,
+(112,'Phía sau nghi can',10,
 83000,
 69000,'View/Resource/book_img/VanHoc-NgheThuat/sach113.jpg',0,0);
 
