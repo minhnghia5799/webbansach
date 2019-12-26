76a25fd8f315316c33b7b1d116c01ae3ca67ebd7
Minh Nghia <minhmeo0507@gmail.com>
2019-12-26 13:06:47 +0700

    123

--- Controller/DirectController.php
+++ Controller/DirectController.php
@@ -9,6 +9,7 @@
         else if ($_GET['page'] == 'contact') require_once SITE_ROOT.'/Controller/ContactController.php';
         else if ($_GET['page'] == 'home') require_once SITE_ROOT.'/Controller/ShopController.php';
         else if ($_GET['page'] == 'login' || $_GET['page'] == 'signup') require_once SITE_ROOT.'/Controller/LoginController.php';
+        else if ($_GET['page'] == 'checkout') require_once SITE_ROOT.'/View/checkout.php';
         else require_once SITE_ROOT.'/View/error.html';
     }
     else require_once SITE_ROOT.'/View/error.html';
